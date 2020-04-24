387e969f57341f9ccf983e4666a61bef8f758b58
Pradeepn29051994 <pradeepn29051994@gmail.com>
2020-04-24 14:18:53 +0530

    Test Folder Structure

---
+++ src/test/java/com/cb/covid/controller/Covid19CasesControllerTest.java
@@ -0,0 +1,19 @@
+package com.cb.covid.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Covid19CasesControllerTest {
+
+	@BeforeEach
+	public void setUp() {
+
+	}
+
+	@Test
+	public void testGetCovid19CasesCount() {
+
+	}
+}
