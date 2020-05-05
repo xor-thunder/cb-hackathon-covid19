6b03553d93e32f948eae2fcb257480b0fefc7944
Pradeepn29051994 <pradeepn29051994@gmail.com>
2020-05-05 22:16:13 +0530

    cloud push changes

---
+++ src/main/java/com/cb/covid/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cb.covid;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Covid19InfectedCasesApplication.class);
+	}
+
+}
